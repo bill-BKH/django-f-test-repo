7d36c789f9d480efc890b7f610ea86751e70750f
Amirhossein-89 <amirhosseinmahm3@gmail.com>
2024-12-26 19:41:54 +0330

    Adding Tasks table to database

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=256) 
+    task_date = models.DateField(auto_now_add=True)
